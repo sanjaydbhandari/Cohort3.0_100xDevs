f45e20ca2aa103126a25ed45ba833f0c14ada96f
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2024-08-25 04:49:35 +0530

    week 4 started

---
+++ Week-4/1_nodeJs.js/index.js
@@ -0,0 +1,12 @@
+// internal package 
+const fs = require('fs');
+const path = require('path')
+
+console.log(path.join(__dirname,'a.txt'));
+console.log(__dirname);
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(1,3))
---
+++ Week-4/1_nodeJs.js/index.mjs
@@ -0,0 +1,5 @@
+import chalk from 'chalk'
+
+console.log(chalk.blue("hello world"));
+console.log(chalk.red.bold("hey sanjay"));
+console.log(chalk.green.underline.bgYellow("this is success message"));
