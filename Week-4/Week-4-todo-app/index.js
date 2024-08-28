732e34487d5362b459228312014a067e402f66e7
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2024-08-28 19:48:21 +0530

    TODO app files

--- Week-4/Week-4-todo-app/index.js
+++ Week-4/Week-4-todo-app/index.js
@@ -1,13 +1,45 @@
-const express = require('express')
-const app = express()
+let data = fetch('./data.json').then((res)=>res.json())
+const bodyELe = document.querySelector('body');
+const model = document.querySelector('.model');
+const containerELe = document.querySelector('.container');
+const CollectionInputEle = document.querySelector('.collection-input-container');
 
-app.get('/',(req,res)=>{
-    res.send('Hello World')
+data.then((collections)=>{
+    collections.map((collection)=>{
+        console.log(collection)
+    })
 })
+// animation
+function slideDown(ele){
+    const SelectEle = document.querySelector(ele);
+    SelectEle.children[0].style.top="50%";
+    SelectEle.style.display='block';
+}
 
-app.get('/get-todo',(req,res)=>{
-    res.json({name:'sanjay',age:''})
-    // res.send('Hello World')
-})
+function closeModel(ele){
+    const SelectEle = document.querySelector(ele);
+    SelectEle.style.display="none";
+}
 
-app.listen(3000)
+window.onclick = function(event){
+    if(event.target == model)
+        model.style.display = 'none';
+}
+
+
+
+// // logic code
+
+// const express = require('express')
+// const app = express()
+
+// app.get('/',(req,res)=>{
+//     res.send('Hello World')
+// })
+
+// app.get('/get-todo',(req,res)=>{
+//     res.json({name:'sanjay',age:''})
+//     // res.send('Hello World')
+// })
+
+// app.listen(3000)
