28bddba8a83d8bc5ca2b408ff292666f016b86f9
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2024-09-07 19:46:52 +0530

    todo api

---
+++ Week-4/Todo_Backend/index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import fs from 'fs'
+import path from 'path'
+
+const app = express();
+const todoFile = './todos.json';
+
+const readTodos=()=>{
+    let todos = fs.readFileSync(todoFile,'utf-8');
+    return JSON.parse(todos);
+}
+
+app.get('/todos',(req,res)=>{
+    let todos = readTodos()
+    res.json(todos)
+})
+
+app.get('/todos:id',(req,res)=>{
+    console.log(req);
+})
+
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log(`Server running on port: ${PORT}`)
+})
