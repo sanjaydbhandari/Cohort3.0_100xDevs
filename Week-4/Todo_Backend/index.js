beaaf5b389089937b7f9a234249fa024a3ba4928
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2024-09-08 19:40:48 +0530

    add files

--- Week-4/Todo_Backend/index.js
+++ Week-4/Todo_Backend/index.js
@@ -1,6 +1,6 @@
-import express from "express";
-import fs from "fs";
-import path from "path";
+import express from "express"
+import fs from 'fs'
+import path from 'path'
 
 const app = express();
 const todoFile = "./todos.json";
@@ -8,33 +8,16 @@ const todoFile = "./todos.json";
 const readTodos = () => {
     let todos = fs.readFileSync(todoFile, "utf-8");
     return JSON.parse(todos);
-};
+}
 
-const response = (status, data, message) => {
-    return {
-        status: status,
-        data: data,
-        message: message,
-    };
-};
+app.get('/todos',(req,res)=>{
+    let todos = readTodos()
+    res.json(todos)
+})
 
-app.get("/todos", (req, res) => {
-    let todos = readTodos();
-    res.json(todos);
-});
-
-app.get("/todos/:id", (req, res) => {
-    try {
-        let todos = readTodos();
-        let todo = todos.find((todo) => todo.id === parseInt(req.params.id));
-        if (!todo) res.status(400).send(response(400, null, "Todo not found"));
-        res.status(200).send(response(200, todo, "Todo found"));
-    } catch (err) {
-        res.status(500).send(response(500, null, "Internal server error"));
-    }
-});
-
-app.get("/todos/:id", (req, res) => {});
+app.get('/todos:id',(req,res)=>{
+    console.log(req);
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
