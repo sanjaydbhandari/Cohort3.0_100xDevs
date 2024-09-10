9a026cc36cdb90d647ccd53fb39e2101e481f1a5
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2024-09-11 00:22:12 +0530

    thats all for today! axios , headers and params and dynamic routes and created my own HTTP server

--- Week-5/Day1/HttpServer/index.js
+++ Week-5/Day1/HttpServer/index.js
@@ -2,18 +2,13 @@ const express = require('express');
 const app = express();
 const axios = require('axios')
 
+// how to use Axios
 app.get('/data',(req,res)=>{
-    const getData = async (url) => {
-        axios.get('https://jsonplaceholder.typicode.com/todos/1')
-            .then(response => {
-                return response.data; // The response from the API
-            })
-            .catch(error => {
-                return {'Error': error};
-            });
+    axios.get('https://jsonplaceholder.typicode.com/todos/.1')
+    .then(response => res.json(response.data))
+    .catch(err=>res.json({"error": err.message}))
     }
-    return getData();
-})
+)
 
 app.get('/add',(req,res)=>{//http://localhost:3000/add?a=10&b=1
     res.json({
@@ -22,7 +17,7 @@ app.get('/add',(req,res)=>{//http://localhost:3000/add?a=10&b=1
 });
 app.get('/addition/:a/:b',(req,res)=>{//http://localhost:3000/addition/10/1
     res.json({
-        "result":parseInt(req.params.a)+parseInt(req.params.b)//request handlers
+        "result":parseInt(req.params.a)+parseInt(req.params.b)//Dynamic routes
     })
 });
 app.get('/sub',(req,res)=>{
