4453e248c46e9b36674e3da3bc50ad29c36d4fa4
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2024-10-01 11:46:49 +0530

    getTodoById working fine

--- Week-7/Todo-app/controllers/todo.controller.js
+++ Week-7/Todo-app/controllers/todo.controller.js
@@ -1,5 +1,6 @@
 const {todoModel} = require('../models/todo.model');
 const {taskSchema} = require('../middleware/validation')
+
 const insertTodo = async (req,res) => {
     try{
         const userId = req.userId;
@@ -14,16 +15,40 @@ const insertTodo = async (req,res) => {
             priority,
             status,
             deadline,
-            // createdAt : Date.now(),
             deleted: false
         });
 
         if(!newTodo)
             res.status(400).json({"message":"Failed to insert Todo"})
-        res.json({message:"Todo inserted Succussfully"})           
+        res.status(200).json({message:"Todo inserted Succussfully"})           
     }catch(err){
         res.status(400).json({"message":err.message})           
     }
 }
 
-module.exports = {insertTodo}
+const updateTodo = async (req,res) =>{
+    try{
+        const userId = req.userId;
+        const validationResult = todoModel.safeParse(req.body)
+        if(!validationResult.success)
+            return res.status(400).json({"error":validationResult.error.format()})
+        console.log(validationResult.data);
+        // const updatedTodo = await taskSchema.findOneAndUpdate({userId,})
+    }catch(err){
+        res.status(400).json({"message":err.message})           
+    }
+}
+
+const fetchTodoById = async (req,res) =>{
+    try{
+        const userId = req.userId;
+        const _id = req.params.id;
+        const todo =await todoModel.findOne({_id:_id});
+        if(!todo)
+            todo=[];
+        res.status(200).json({message:"Todo fetch Succussfully",data:todo})           
+    }catch(err){
+        res.status(400).json({"message":err.message})           
+    }
+}
+module.exports = {insertTodo,updateTodo,fetchTodoById}
--- Week-7/Todo-app/routes/todo.route.js
+++ Week-7/Todo-app/routes/todo.route.js
@@ -1,14 +1,10 @@
 const {Router} = require("express");
 const {auth} = require('../middleware/auth')
-const {insertTodo} = require('../controllers/todo.controller')
+const {insertTodo,fetchTodoById,updateTodo} = require('../controllers/todo.controller')
 const todoRouter = Router();
 todoRouter.get('/',auth,insertTodo)
-todoRouter.get('/:id',(req,res)=>{
-    res.json({message:"todos by id"})
-})
-todoRouter.put('/:id',(req,res)=>{
-    res.json({message:"todos list"})
-})
+todoRouter.get('/:id',fetchTodoById)
+todoRouter.put('/:id',updateTodo)
 todoRouter.delete('/:id',(req,res)=>{
     res.json({message:"todos list"})
 })
