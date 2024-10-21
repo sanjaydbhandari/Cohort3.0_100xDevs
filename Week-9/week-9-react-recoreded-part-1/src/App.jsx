927864026525f9080805563a722eac007452564d
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2024-10-21 23:06:32 +0530

    add item component

--- Week-9/week-9-react-recoreded-part-1/src/App.jsx
+++ Week-9/week-9-react-recoreded-part-1/src/App.jsx
@@ -1,29 +1,32 @@
 import './App.css'
 import Notification from "./Notification"
-import profileImage from "./assets/profile-image.png"
+import profileImage from "./assets/profile-image.png";
+
+let item=[{
+  id:1,
+  isVeg:true,
+  itemName:"Paneer Butter Masala",
+  itemDesp:"Paneer Butter Masala",
+  itemPrice:279,
+  itemImage:"",
+}]
 function App() {
   return (
-    <PostComponent />
+    <PostComponent key={item.id} isVeg={item.isVeg} itemName={item.itemName} itemDesp={item.itemDesp} itemPrice={item.itemPrice} itemImage={item.itemImage}/>
   )
 }
 
-function PostComponent(){
+function PostComponent([isVeg,itemName,itemDesp,itemPrice]){
   return(
-    <div className="postComponent" style={{backgroundColor:"#e0e0e0",width:"400px"}}>
-      <div className="post-content" style={{padding:"1rem 2rem"}}>
-
-        <div className="profile-image" style={{display:"flex",flexDirection:"row"}}>
-        <img src={profileImage} style={{width:"2rem",height:"2rem",borderRadius:"50%"}}/>
-        <div className="user-details" style={{display:'flex',padding:"0rem 1rem",border:"1px solid #000",flexDirection:"column"}}>
-          <span className='username' style={{fontSize:"1rem"}}>Sanjay Bhandari</span>
-          <span className='followers-count' style={{fontSize:".6rem"}}>1000 followers</span>
-          <span className='post-timestamp' style={{fontSize:".6rem"}}>12 min</span>
-          <span className='post-timestamp' style={{fontSize:".6rem"}}>12 min</span>
+    <div className="itemComponent" style={{padding:"1rem 1rem",backgroundColor:"#e0e0e0",width:"100vw",height:"100vh",border:"1px solid #000",display:"flex",flexDirection:"column",rowGap:"2rem"}}>
+      <div className="item-container" style={{backgroundColor:"#fff",padding:".5rem .5rem",alignItems:"center",display:"flex",justifyContent:"space-between",border:"1px solid #000",borderRadius:"1rem"}}>
+        <div className="user-details" style={{display:'flex',flexDirection:"column"}}>
+          <span className='item-type' style={{fontSize:".8rem"}}>{isVeg ? "veg" : "nonveg"}</span>
+          <span className='item-name' style={{fontSize:"1rem",fontWeight:"600"}}>{itemName}</span>
+          <span className='item-description' style={{fontSize:".8rem",paddingTop:".6rem"}}>{itemDesp}</span>
+          <span className='item-price' style={{fontSize:".8rem",fontWeight:"600",paddingTop:".4rem"}}>Rs. {itemPrice}</span>
         </div>
-      </div>
-        <div className="post-description">
-          100xdev hackathon
-        </div>
+        <img src={profileImage} style={{width:"5rem",height:"5rem",borderRadius:".8rem"}}/>
       </div>
     </div>
   )
