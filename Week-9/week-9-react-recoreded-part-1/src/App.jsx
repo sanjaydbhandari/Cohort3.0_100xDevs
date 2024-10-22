51b015b5f2fefe68ace426f563bcaea4c7305fe6
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2024-10-22 23:20:54 +0530

    react week 9 completed

--- Week-9/week-9-react-recoreded-part-1/src/App.jsx
+++ Week-9/week-9-react-recoreded-part-1/src/App.jsx
@@ -4,26 +4,41 @@ import Notification from "./Notification"
 import profileImage from "./assets/profile-image.png";
 
 function App() {
-let  item = [{
-  id:1,
-  isVeg:true,
-  itemName:"Paneer Butter Masala",
-  itemDesp:"Paneer Butter Masala",
-  itemPrice:279,
-  itemImage:"",
-}];
+  const [items,setItems] = useState([{
+    id:1,
+    isVeg:true,
+    name:"Paneer Butter Masala",
+    desp:"Paneer Butter Masala",
+    price:279,
+    image:"",
+  },]);
 
-  item=item[0];
+  function add(){
+    setItems([
+      ...items,{
+        id:1,
+        isVeg:true,
+        name:"Chiken Butter Masala",
+        desp:"Chiken Butter Masala",
+        price:499,
+        image:"",
+      }
+    ])
+  }
+
   return (
   <div className="itemComponent" style={{padding:"1rem 1rem",backgroundColor:"#e0e0e0",width:"100vw",height:"100vh",border:"1px solid #000",display:"flex",flexDirection:"column",rowGap:"2rem"}}>
-    <ItemComponent key={item.id} isVeg={item.isVeg} itemName={item.itemName} itemDesp={item.itemDesp} itemPrice={item.itemPrice} itemImage={item.itemImage}/>
+      {
+        items.map((item)=><ItemComponent key={item.id} isVeg={item.isVeg} itemName={item.name} itemDesp={item.desp} itemPrice={item.price} itemImage={item.image}/>)
+      }
+      <button onClick={add}>Add</button>
     </div>
   )
 }
 
-export function ItemComponent({id,isVeg,itemName,itemDesp,itemPrice}){
+export function ItemComponent({isVeg,itemName,itemDesp,itemPrice}){
   return(
-      <div className="item-container" key={id} style={{backgroundColor:"#fff",padding:".5rem .5rem",alignItems:"center",display:"flex",justifyContent:"space-between",border:"1px solid #000",borderRadius:"1rem"}}>
+      <div className="item-container" style={{backgroundColor:"#fff",padding:".5rem .5rem",alignItems:"center",display:"flex",justifyContent:"space-between",border:"1px solid #000",borderRadius:"1rem"}}>
         <div className="user-details" style={{display:'flex',flexDirection:"column"}}>
           <span className='item-type' style={{fontSize:".8rem"}}>{isVeg ? "veg" : "nonveg"}</span>
           <span className='item-name' style={{fontSize:"1rem",fontWeight:"600"}}>{itemName}</span>
